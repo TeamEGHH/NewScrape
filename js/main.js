2ce36e84ccf718940cf161a08474d70877f6bbb0
Hjörtur Larsen <91hjortur@gmail.com>
2015-03-23 22:11:56 +0000

    filter placeholder

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Global variables. Marked with _g wich indicates that it
  * is a global variable.
  */
- var g_menu, g_wall, g_menu_pos, g_image_header;
+var g_menu, g_menu_pos, g_image_header;
 
 $(document).ready(function() {
     /*
@@ -12,8 +12,10 @@ $(document).ready(function() {
     function fetch_and_display() {
         $.get("/loadnews", function(data) {
             var json = $.parseJSON(data);
-            success:
+            start:
+                success:
                 writeHTML(json).done(function() {
+
                     htmlEffects(json);
                 });
         });
@@ -24,6 +26,7 @@ $(document).ready(function() {
      * Writes data to html using Handlebars.js
      */
     function writeHTML(data) {
+        //Edit time format
         for (var i = 0; i < data.length; i++) {
             data[i].time = moment(data[i].time).fromNow();
         }
@@ -39,6 +42,10 @@ $(document).ready(function() {
      * has been loaded.
      */
     function htmlEffects(data) {
+        g_menu = $('#header'); //The header div
+        g_menu_pos = g_menu.offset(); //The position of the header div
+        g_image_header = $('.image-header');
+
         g_wall.container.find('.brick img').load(function() {
             g_wall.fitWidth();
         });
@@ -46,10 +53,6 @@ $(document).ready(function() {
         //This is done to avoid prevoius odd behaviour of the medium-and-time class
         //positioning. So we add the position after everything has been loaded.
         $('.medium-and-time').addClass('medium-and-time-pos');
-
-        g_menu = $('#header'); //The header div
-        g_menu_pos = g_menu.offset(); //The position of the header div
-        g_image_header = $('.image-header');
     }
 
     /*
@@ -68,15 +71,21 @@ $(document).ready(function() {
             //Make header fixed to top while scrolling
             function headerScroll() {
                 if ($(this).scrollTop() >= g_menu_pos.top) {
-                    g_menu.addClass('header-container');
-                    g_image_header.addClass('image-header-scroll');
+                    g_menu.addClass('header-container', 2000, 'easeInBack');
+                    g_image_header.addClass('image-header-scroll', 2000, 'easeInBack');
                 } else if ($(this).scrollTop() <= g_menu_pos.top) {
-                    g_menu.removeClass('header-container');
-                    g_image_header.removeClass('image-header-scroll', 1000, "easeInBack");
+                    g_menu.removeClass('header-container', 2000, 'easeInBack');
+                    g_image_header.removeClass('image-header-scroll', 2000, "easeInBack");
                 }
             }
             dataLoad();
             headerScroll();
+        });
+
+        $(window).resize(function() {
+            g_menu = $('#header'); //The header div
+            g_menu_pos = g_menu.offset(); //The position of the header div
+            g_image_header = $('.image-header');
         });
     }
 
