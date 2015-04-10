a69d000416f4599db27c24f9fbbbcb232e13f701
Hjörtur Larsen <91hjortur@gmail.com>
2015-04-10 15:37:21 +0000

    ajxcomplete fall ofl.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+
+    var $loading = $('#loadingDiv').hide();
+
+    $(document)
+        .ajaxStart(function() {
+            $loading.show();
+        })
+        .ajaxStop(function() {
+            $loading.hide();
+        });
+
     var wall = new freewall("#freewall");
 
     wall.reset({
@@ -10,6 +21,7 @@ $(function() {
             wall.fitWidth();
         }
     });
+
     /*
      * Fetch data from mongodb and append the data
      * to index.html
@@ -17,9 +29,7 @@ $(function() {
     function fetch_and_display() {
         $.get("/loadnews", function(data) {
             var json = $.parseJSON(data);
-            success: writeHTML(json).done(function() {
-                afterHTMLLoad();
-            });
+            success: writeHTML(json);
         });
     }
 
@@ -38,31 +48,26 @@ $(function() {
             wall.appendBlock(template(item));
             wall.fitWidth();
         });
-        return $.ajax();
     }
 
     /*
-     * Helper function for fetch_and_display:
-     * jQuery effects, ran after all the html data
-     * has been loaded.
+     * Stuff to do when ajax is request is complete
      */
-    function afterHTMLLoad() {
+    $(document).ajaxComplete(function() {
+
         wall.container.find('.brick img').load(function() {
             wall.fitWidth();
         });
-
-        
-        g_menu_pos = g_menu.offset(); //The position of the header div
 
         //This is done to avoid prevoius odd behaviour of the medium-and-time class
         //positioning. So we add the position after everything has been loaded.
         $('.medium-and-time').addClass('medium-and-time-pos');
 
         $(window).trigger('resize');
-    }
+    });
 
     /*
-     * Global variables. Marked with _g wich indicates that it
+     * Global variables. Marked with g_ wich indicates that it
      * is a global variable.
      */
     var g_menu, g_menu_pos, g_image_header;
@@ -70,47 +75,33 @@ $(function() {
     $(document).ready(function() {
         g_menu = $('#header'); //The header div
         g_image_header = $('.image-header');
+        g_menu_pos = g_menu.offset(); //The position of the header div
 
         /*
          * Listens for desired events
          */
-        function listener() {
-            //Listen for scroll
-            $(window).scroll(function() {
-                //Load more data if we hit the bottom of the page
-                function dataLoad() {
-                    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-                        fetch_and_display();
-                    }
-                }
 
-                //Make header fixed to top while scrolling
-                function headerScroll() {
-                    if ($(this).scrollTop() >= g_menu_pos.top) {
-                        g_menu.addClass('header-container', 2000, 'easeInBack');
-                        g_image_header.addClass('image-header-scroll', 2000, 'easeInBack');
-                    } else if ($(this).scrollTop() <= g_menu_pos.top) {
-                        g_menu.removeClass('header-container', 2000, 'easeInBack');
-                        g_image_header.removeClass('image-header-scroll', 2000, "easeInBack");
-                    }
-                }
-                dataLoad();
-                headerScroll();
-            });
+        $(window).scroll(function() {
+            //Load more data if we hit the bottom of the page
+            if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+                fetch_and_display();
+            }
 
-            $(window).resize(function() {
-                g_menu = $('#header'); //The header div
-                g_menu_pos = g_menu.offset(); //The position of the header div
-                g_image_header = $('.image-header');
-            });
-        }
+            //Make header fixed to top while scrolling
+            if ($(this).scrollTop() >= g_menu_pos.top) {
+                g_menu.addClass('header-container', 2000, 'easeInBack');
+                g_image_header.addClass('image-header-scroll', 2000, 'easeInBack');
+            } else if ($(this).scrollTop() <= g_menu_pos.top) {
+                g_menu.removeClass('header-container', 2000, 'easeInBack');
+                g_image_header.removeClass('image-header-scroll', 2000, "easeInBack");
+            }
+        });
 
         /*
          * Main function to keep track of function calling
          */
         function main() {
             fetch_and_display();
-            listener();
         }
 
         //Kick things off
